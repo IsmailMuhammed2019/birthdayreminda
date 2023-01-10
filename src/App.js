f9e89d5b9a4e35030500fbf7caa6388bb6d3d321
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-10 15:25:08 +0100

    add clear all button

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import data from './data'
 import List from './List'
 function App() {
   const [people, setPeople] = useState(data)
+
   return (
-  <main>
-    <section className='container'>
-      <h3>{people.length} Birthdays Today</h3>
-      <List />
-    </section>
-  </main>
-)
+    <main>
+      <section className='container'>
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people}/>
+        <button onClick={() => setPeople([])}>Clear All</button>
+      </section>
+    </main>
+  )
 }
 
 export default App
