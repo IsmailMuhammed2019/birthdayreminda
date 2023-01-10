37758909bd73a916a39d9684f68738a4bfa8e5fe
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-10 15:43:33 +0100

    update list.js props with an object

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import data from './data'
 import List from './List'
-function App() {
-  const [people, setPeople] = useState(data)
 
+const App = () => {
+  const [people, setPeople] = useState(data)
   return (
     <main>
       <section className='container'>
-        <h3>{people.length} Birthdays Today</h3>
+        <h3><strong>{people.length}</strong> Birthdays Today</h3>
         <List people={people}/>
         <button onClick={() => setPeople([])}>Clear All</button>
       </section>
