df589e681a402833992cc620fdde95e7d86e7642
ismailmuhammed2019 <ismailmuhammed2019@gmail.com>
2023-01-11 15:58:36 +0100

    update project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React, {useState} from 'react'
-import data from './data'
+import React, { useState } from 'react'
 import List from './List'
+import data from './data'
 
 const App = () => {
   const [people, setPeople] = useState(data)
   return (
     <main>
       <section className='container'>
-        <h3><strong>{people.length}</strong> Birthdays Today</h3>
-        <List people={people}/>
+        <h3>{people.length} Birthdays Today</h3>
+        <List people={people} />
         <button onClick={() => setPeople([])}>Clear All</button>
       </section>
     </main>
--- src/List.js
+++ src/List.js
@@ -3,18 +3,18 @@ import React from 'react'
 const List = ({people}) => {
   return (
     <>
-     {people.map((person) => {
-      const {id, name, age, image} = person
-      return (
-       <article className='person' key={id}>
-        <img src={image} alt={name} />
-        <div>
-         <h4>{name}</h4>
-         <h4>{age} years</h4>
-        </div>
-       </article>
-      )
-     })}
+      {people.map((person) => {
+        const {id, name, age, image} = person
+        return (
+          <article className='person' key={id}>
+            <img src={image} alt={name} />
+            <div>
+              <h4>{name}</h4>
+              <p>{age} years</p>
+            </div>
+          </article>
+        )
+      })}
     </>
   )
 }
